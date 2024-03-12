75f27edc3241a65c5743e923c15d23b250daa8bd
imranFswd <imranfswd@gmail.com>
2024-03-12 11:56:08 +0600

    Minor update

--- src/components/main/testimonial/TestimonialCards.tsx
+++ src/components/main/testimonial/TestimonialCards.tsx
@@ -74,7 +74,7 @@ function TestimonialCards() {
         <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
 
             <div>
-                <h2 className="text-3xl font-bold text-center mb-8 z-10 capitalize selection:bg-teal-600">Testimonials</h2>
+                <h2 className="text-3xl font-bold text-center mb-8 z-10 capitalize ">Testimonials</h2>
             </div>
 
             <div className="flex justify-center w-full overflow-hidden px-4 sm:px6 lg:px-8">
